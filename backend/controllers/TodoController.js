7954ce10aba9202a2406d5b13d9246040a11cf1d
NdekoCode <arickbulakali@gmail.com>
2022-10-19 17:23:52 +0200

    refactor: route add new Todo in backend

--- backend/controllers/TodoController.js
+++ backend/controllers/TodoController.js
@@ -1,30 +1,54 @@
 import { TodoModel } from "../models/Todos.js";
 export default class TodoController {
   addTodo(req, res) {
-    const todo = new TodoModel({
-      content: "Apprendre express",
-      completed: false,
-    });
-    todo
-      .save()
-      .then(() => {
-        return res.status(200).send({
-          alert: {
-            message: "Todos Ajouter avec succés",
-            type: "success",
-            statusCode: 201,
-          },
-        });
-      })
-      .catch(() =>
-        res.status(500).send({
-          alert: {
-            message: "Erreur lors de l'enregistrement des données",
-            type: "danger",
-            statusCode: 500,
-          },
+    if (req.body) {
+      const newTodos = {
+        content: req.body.content,
+        completed: false,
+      };
+
+      const todo = new TodoModel(newTodos);
+      return TodoModel.exists({ content: req.body.content })
+        .then((data) => {
+          if (!data) {
+            return todo
+              .save()
+              .then(() => {
+                return res.status(200).send({
+                  alert: {
+                    message: "Todos Ajouter avec succés",
+                    type: "success",
+                    statusCode: 201,
+                  },
+                });
+              })
+              .catch(() =>
+                res.status(500).send({
+                  alert: {
+                    message: "Erreur lors de l'enregistrement des données",
+                    type: "danger",
+                    statusCode: 500,
+                  },
+                })
+              );
+          }
+          return res.status(409).send({
+            alert: {
+              message: "La tache existe déjà",
+              type: "danger",
+              statusCode: 409,
+            },
+          });
         })
-      );
+        .catch(() => {});
+    }
+    return res.status(400).send({
+      alert: {
+        message: "Erreur lors de l'enregistrement des données",
+        type: "danger",
+        statusCode: 400,
+      },
+    });
   }
   getTodos(req, res) {
     TodoModel.find((err, items) => {
