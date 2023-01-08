cc19bde704a31a69358417e9227a4e073f0f4f7c
Ndekocode <arickbulakali@gmail.com>
2023-01-08 15:06:42 +0200

    feat: refactor: add new todo function

--- backend/controllers/TodoController.js
+++ backend/controllers/TodoController.js
@@ -1,47 +1,44 @@
 import { TodoModel } from "../models/Todos.js";
 export default class TodoController {
-  addTodo(req, res) {
+  async addTodo(req, res) {
     if (req.body) {
       const newTodos = {
         content: req.body.content,
         completed: false,
       };
-
-      const todo = new TodoModel(newTodos);
-      return TodoModel.exists({ content: req.body.content })
-        .then((data) => {
-          if (!data) {
-            return todo
-              .save()
-              .then(() => {
-                return res.status(200).send({
-                  alert: {
-                    message: "Todos Ajouter avec succés",
-                    type: "success",
-                    statusCode: 201,
-                  },
-                });
-              })
-              .catch(() =>
-                res.status(500).send({
-                  alert: {
-                    message: "Erreur lors de l'enregistrement des données",
-                    type: "danger",
-                    statusCode: 500,
-                  },
-                })
-              );
-          }
-          return res.status(409).send({
+      try {
+        const todo = new TodoModel(newTodos);
+        const data = await TodoModel.findOne({ content: req.body });
+        if (!data) {
+          await todo.save();
+          return res.status(200).send({
             alert: {
-              message: "La tache existe déjà",
-              type: "danger",
-              statusCode: 409,
+              message: "Todos Ajouter avec succés",
+              type: "success",
+              statusCode: 201,
             },
           });
-        })
-        .catch(() => {});
+        }
+
+        return res.status(409).send({
+          alert: {
+            message: "La tache existe déjà",
+            type: "danger",
+            statusCode: 409,
+          },
+        });
+      } catch (error) {
+        res.status(500).send({
+          alert: {
+            message:
+              "Erreur lors de l'enregistrement des données " + error.message,
+            type: "danger",
+            statusCode: 500,
+          },
+        });
+      }
     }
+
     return res.status(400).send({
       alert: {
         message: "Erreur lors de l'enregistrement des données",
