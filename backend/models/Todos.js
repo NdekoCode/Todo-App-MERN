557ac2e838879f38ad1a2776a3861d3275ae9127
Ndekocode <arickbulakali@gmail.com>
2023-01-08 15:07:43 +0200

    refactor: todo model schema

--- backend/models/Todos.js
+++ backend/models/Todos.js
@@ -1,22 +1,17 @@
-import { Schema, model } from "mongoose";
-const TodoSchema = new Schema({
-  content: {
-    type: String,
-    required: true,
+import { model, Schema } from "mongoose";
+const TodoSchema = new Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    completed: {
+      type: Boolean,
+      default: false,
+    },
   },
-  completed: {
-    type: Boolean,
-    default: false,
-  },
-  created_at: {
-    type: Date,
-    default: Date.now(),
-    required: true,
-  },
-  updated_at: {
-    type: Date,
-  },
-});
+  { timestamps: true }
+);
 
 /**
  * @type {collection}
