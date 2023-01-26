be5b3fe598e921601c34a3b4f1c5299a44d9ccd2
Ndekocode <arickbulakali@gmail.com>
2023-01-26 19:35:23 +0200

    fix: DB error on failure request

--- backend/controllers/UsersController.js
+++ backend/controllers/UsersController.js
@@ -3,16 +3,26 @@ import Alert from "../utils/Alert.js";
 
 export default class UsersControllers {
   async getAllUsers(req, res) {
-    const users = await UserModel.find().sort({ createdAt: -1 }).exec();
-    return res.status(200).json(users || []);
+    try {
+      const users = await UserModel.find().sort({ createdAt: -1 }).exec();
+      return res.status(200).json(users || []);
+    } catch (error) {
+      const alert = new Alert(req, res);
+      return alert.danger(error.message, 500);
+    }
   }
   async getSingleUser(req, res) {
-    const id = req.params.id;
-    const user = await UserModel.findById(id);
-    if (user) {
-      return res.status(200).json(user);
+    try {
+      const id = req.params.id;
+      const user = await UserModel.findById(id);
+      if (user) {
+        return res.status(200).json(user);
+      }
+      const alert = new Alert(req, res);
+      return alert.danger("Cet utilisateur n'existe pas");
+    } catch (error) {
+      const alert = new Alert(req, res);
+      return alert.danger(error.message, 500);
     }
-    const alert = new Alert(req, res);
-    return alert("Cet utilisateur n'existe pas");
   }
 }
