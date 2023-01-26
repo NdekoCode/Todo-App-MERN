a3d3a5af1b6e4a804e22200bb8a12c38c53d74a9
Ndekocode <arickbulakali@gmail.com>
2023-01-26 19:33:32 +0200

    feat: get a single user

--- backend/controllers/UsersController.js
+++ backend/controllers/UsersController.js
@@ -1,8 +1,18 @@
 import UserModel from "../models/UserModel.js";
+import Alert from "../utils/Alert.js";
 
 export default class UsersControllers {
   async getAllUsers(req, res) {
     const users = await UserModel.find().sort({ createdAt: -1 }).exec();
     return res.status(200).json(users || []);
+  }
+  async getSingleUser(req, res) {
+    const id = req.params.id;
+    const user = await UserModel.findById(id);
+    if (user) {
+      return res.status(200).json(user);
+    }
+    const alert = new Alert(req, res);
+    return alert("Cet utilisateur n'existe pas");
   }
 }
--- backend/routes/userRoute.routes.js
+++ backend/routes/userRoute.routes.js
@@ -3,4 +3,5 @@ import UsersControllers from "../controllers/UsersController.js";
 const userRouter = Router();
 const userCTRL = new UsersControllers();
 userRouter.get("/users", userCTRL.getAllUsers);
+userRouter.get("/users/:id", userCTRL.getSingleUser);
 export default userRouter;
